506389f1837304bc41fb523cbc2692e86ceabb84
Paul <paul@example.com>
2026-05-07 12:29:44 -0400

    feat: change session strategy to database

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,6 @@ import authConfig from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  session: { strategy: "database" },
   ...authConfig,
 })
