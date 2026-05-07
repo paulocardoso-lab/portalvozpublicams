4c95ea687acfdc2b832924bfa4b40fe0344d95cf
Paul <paul@example.com>
2026-05-07 14:03:20 -0400

    refactor: use standard AUTH_GOOGLE_ID and AUTH_GOOGLE_SECRET variable names

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -5,8 +5,8 @@ import Resend from "next-auth/providers/resend"
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
     Resend({
       apiKey: process.env.RESEND_API_KEY,
